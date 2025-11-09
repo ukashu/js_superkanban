1a69bde7c275b300c747609a771a7a0a1cd2fe1b
LexSnow <108388996+LexSnow@users.noreply.github.com>
2025-11-09 13:04:03 +0100

    corrected formatting for routes and added them to server.js

--- backend/projects/project.routes.js
+++ backend/projects/project.routes.js
@@ -1,14 +1,14 @@
 import express from "express"
-import {getProjects, createProject, editProject, deleteProject} from "./project.controllers.js"
+import { getProjects, createProject, editProject, deleteProject } from "./project.controllers.js"
 
-const router = express.Router({mergeParams: true})
+const router = express.Router({ mergeParams: true })
 
 router.route("/:projectId")
-.put(editProject)
-.delete(deleteProject)
+    .put(editProject)
+    .delete(deleteProject)
 
 router.route("/")
-.get(getProjects)
-.post(createProject)
+    .get(getProjects)
+    .post(createProject)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import errorHandler from "./middleware/error.middleware.js"
 import userRoutes from "./users/user.routes.js"
+import projectRoutes from "./projects/project.routes.js"
 
 const app = express()
 const PORT = Number(process.env.PORT) || 5000;
@@ -10,6 +11,7 @@ app.get("/", (req, res)=>{
 })
 
 app.use("/api/users", userRoutes)
+app.use("/api/projects", projectRoutes)
 
 app.use(errorHandler)
 

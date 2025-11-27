aa3c0d5008d9f45bc15648fc2cbc40a6306330ad
ukashu <lukaszfilipiuk123@gmail.com>
2025-11-27 14:11:20 +0100

    +generic routes

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
+    <nav>
+        <RouterLink to="/users">Go to User</RouterLink>
+        <RouterLink to="/projects/:projectId">Go to Project</RouterLink>
+    </nav>
+    <main>
+        <RouterView />
+    </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ client/src/views/UserHomePage.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { ref, onMounted } from "vue"
+
+</script>
+
+<template>
+    <p>I AM THE USER HOME PAGE</p>
+</template>

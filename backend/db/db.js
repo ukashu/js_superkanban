d7f5060b8fff64749093d6c80d3e69e631244c7a
ukashu <lukaszfilipiuk123@gmail.com>
2025-11-27 12:39:59 +0100

    +db helper functions

---
+++ backend/db/db.js
@@ -0,0 +1,28 @@
+import db from "./dbInit.js"
+
+export function dbGet(sql, params = []) {
+    return new Promise((resolve, reject) => {
+        db.get(sql, params, (err, row) => {
+            if (err) reject(err)
+            else resolve(row)
+        })
+    })
+}
+
+export function dbAll(sql, params = []) {
+    return new Promise((resolve, reject) => {
+        db.all(sql, params, (err, rows) => {
+            if (err) reject(err)
+            else resolve(rows)
+        })
+    })
+}
+
+export function dbRun(sql, params = []) {
+    return new Promise((resolve, reject) => {
+        db.run(sql, params, function (err) {
+            if (err) reject(err)
+            else resolve({ lastID: this.lastID, changes: this.changes })
+        })
+    })
+}

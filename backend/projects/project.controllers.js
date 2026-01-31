9130f2782d2de78f3238366a6bf1800a201ac68d
ukashu <lukaszfilipiuk123@gmail.com>
2026-01-31 18:06:38 +0100

    -useless console log

--- backend/projects/project.controllers.js
+++ backend/projects/project.controllers.js
@@ -15,7 +15,6 @@ export const getProjects = async (req, res, next) => {
 }
 
 export const getProjectById = async (req, res, next) => {
-    console.log("HIT THE ENDPOINT!!!!!")
     const projectId = Number(req.params.projectId)
     try {
         const project = await dbGet(
